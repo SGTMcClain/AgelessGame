96db8858deea8cb1f58cd17fe285471221ebf9a9
jgarcy1080 <jgarcy1080@gmail.com>
2015-10-08 16:43:22 -0400

    Playing with Scripts (Not Finished Yet)

---
+++ Assets/Punching.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            GetComponent<Animation>().Play("punch");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/PunchButtonJG.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PunchButtonJG : MonoBehaviour
+{
+    public Collider myCollider1;
+    public Collider myCollider2;
+    private Animator myAnimator;
+
+
+  
+  void Start()
+    {
+        myAnimator = GetComponent <Animator>();
+        myCollider1 = GetComponent<Collider>();
+        myCollider2 = GetComponent<Collider>();
+    }
+
+
+     void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.V) && !animation.IsPlaying(punchAnimation))
+        {
+            //turn on renderers and start playing the animation
+            myCollider1.enabled = true;
+            myCollider2.enabled = true;
+
+
+            //play the animation
+            GetComponent<Animation>().Play("punch");
+
+        else
+        {
+            //ensure animation stops
+            animation.Stop(punchAnimation);
+
+                //disable renderers
+                myCollider1.enabled = false;
+                myCollider2.enabled = false;
+            }
+
+    }
+    
+
+
+
+
+
+}
