d0e1ddf707ae7f6a08a7c06ccc0627ef05e383f1
soliddave84 <soliddave84@users.noreply.github.com>
2015-10-28 18:27:54 -0400

    Added breaking platform script

---
+++ Assets/Scripts/BreakingPlatform.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakingPlatform : MonoBehaviour
+{
+    // platorm breaks when Teen and elderly walk onto it.
+    
+    public GameObject modelTeen;
+    public GameObject modelElderly;
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+
+
+
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        if  (other.gameObject.name == modelTeen.name || other.gameObject.name == modelElderly.name)
+        {
+           gameObject.SetActive(false);
+        }
+       
+    }
+}
+
+    
