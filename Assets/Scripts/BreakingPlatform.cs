a4771d93acd6bb83c4ccaa46759246ae53de46fc
soliddave84 <soliddave84@users.noreply.github.com>
2015-10-29 00:24:40 -0400

    Finished the basic Breaking Platform code

--- Assets/Scripts/BreakingPlatform.cs
+++ Assets/Scripts/BreakingPlatform.cs
@@ -4,31 +4,98 @@ using System.Collections;
 public class BreakingPlatform : MonoBehaviour
 {
     // platorm breaks when Teen and elderly walk onto it.
-    
+
+   //Instructions: Create an Empty Object add a trigger collider. Name the trigger to anything you wish. Make the platform you want to disappear when stepped ona child of the Empty Object. 
+
     public GameObject modelTeen;
     public GameObject modelElderly;
+    public GameObject mPlatform;  //set the platform you want to disappear in inspector.
+    
+
+
+
+
 
 
     void Start()
+
     {
         
     }
-
 
     void Update()
     {
 
 
 
+     
+
+
     }
-    void OnCollisionEnter(Collision other)
+
+
+
+
+
+    //breaks the platform if Teen or Elderly
+    void OnTriggerEnter(Collider other)
     {
-        if  (other.gameObject.name == modelTeen.name || other.gameObject.name == modelElderly.name)
+        if (other.gameObject.name == modelTeen.name || other.gameObject.name == modelElderly.name)
         {
-           gameObject.SetActive(false);
+
+
+
+
+            StartCoroutine(waitToBreak());
+
+
+
+
+
+
         }
-       
     }
-}
+
+    //reforms the platform after some time
+
+        void OnTriggerExit(Collider other) { 
+
+
+        {
+            StartCoroutine(waitToReform());
+        }
+    }
+   
+
+
+
 
     
+
+
+    IEnumerator waitToBreak()
+    {
+
+        yield return new WaitForSeconds(3);
+
+        mPlatform.gameObject.SetActive(false);
+        
+
+        
+
+        
+
+
+    }
+
+
+    IEnumerator waitToReform()
+
+    {
+        yield return new WaitForSeconds(5);
+        mPlatform.gameObject.SetActive(true);
+        
+    }
+
+
+}
