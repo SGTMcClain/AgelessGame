0da8d033b68adad1aeace94bc6a955ba05e34e1a
Nathan McClain <n8mcclain@gmail.com>
2015-11-05 14:25:41 -0500

    Start of a LeaderBoard
    Leaderboard created but not yet active in scene.  Still need work with
    PlayerPrefs

---
+++ Assets/Scripts/PlayerScoreList.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerScoreList : MonoBehaviour {
+
+    public GameObject playerScoreEntryPrefab;
+
+    ScoreManager scoreManager;
+
+    int lastChangeCounter;
+     
+    // Use this for initialization
+	void Start () {
+
+        scoreManager = GameObject.FindObjectOfType<ScoreManager>();
+
+        lastChangeCounter = scoreManager.GetChangeCounter();
+       
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (scoreManager == null)
+        {
+            Debug.LogError("You  forgot to add the score manager component to a game object!");
+            return;
+        }
+
+        if(scoreManager.GetChangeCounter() == lastChangeCounter)
+        {
+            //No change since last update!
+            return;
+        }
+
+        lastChangeCounter = scoreManager.GetChangeCounter();
+
+        while (this.transform.childCount > 0)
+        {
+            Transform c = this.transform.GetChild(0);
+            c.SetParent(null); //Became Batman
+            Destroy(c.gameObject);
+        }
+
+        string[] names = scoreManager.GetPlayerNames("time"); //parenthesis can be empty or have the string name of the property you want to sort
+
+        foreach (string name in names)
+        {
+            GameObject go = (GameObject)Instantiate(playerScoreEntryPrefab);
+            go.transform.SetParent(this.transform);
+            go.transform.Find("Username").GetComponent<Text>().text = name;
+            go.transform.Find("Time").GetComponent<Text>().text = scoreManager.GetScore(name, "time").ToString();
+            go.transform.Find("Trials").GetComponent<Text>().text = scoreManager.GetScore(name, "trials").ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq; //Check documentation on this
+
+public class ScoreManager : MonoBehaviour {
+
+    //We need some sort of map that goes from a USERNAME to a SCORE
+    //The map we are building is going to look like:
+    //
+    // LIST OF USERS -> A User -> LIST OF SCORES for that user
+    //
+
+    Dictionary<string, Dictionary<string, int> > playerScores;
+    int changeCounter = 0;
+
+    void Start()
+    {
+        SetScore("quill18", "time", 9000);
+        SetScore("quill18", "trials", 15);
+
+        SetScore("Joel2015", "time", 2000);
+        SetScore("Joel2015", "trials", 9001);
+
+        SetScore("AAAAAAA", "time", 0);
+        SetScore("AAAAAAA", "trials", 0);
+
+        SetScore("BBBBBBB", "time", 0);
+
+        SetScore("CCCCCC", "time", 4321);
+        SetScore("CCCCCC", "trials", 1234);
+
+
+
+        Debug.Log(GetScore("quill18", "kills"));
+    }
+
+    void Init()
+    {
+        if (playerScores != null)
+            return;
+
+        playerScores = new Dictionary<string, Dictionary<string, int>>();
+    }
+
+    public int GetScore(string username, string scoreType)
+    {
+        Init();
+
+        if(playerScores.ContainsKey(username) == false)
+        {
+            //we have no score record for this username
+            return 0;
+        }
+
+        if (playerScores[username].ContainsKey(scoreType) == false)
+        {
+            return 0;
+        }
+
+        return playerScores[username][scoreType];
+    }
+
+    public void SetScore(string username, string scoreType, int value)
+    {
+        Init();
+
+        changeCounter++;
+
+        if(playerScores.ContainsKey(username) == false)
+        {
+            playerScores[username] = new Dictionary<string, int>();
+        }
+
+        playerScores[username][scoreType] = value;
+    }
+    
+    public void ChangeScore(string username, string scoreType, int amount)
+    {
+        Init();
+
+        int currScore = GetScore(username, scoreType);
+        SetScore(username, scoreType, currScore + amount);
+    }
+	
+    public string[] GetPlayerNames()
+    {
+        Init();
+        return playerScores.Keys.ToArray();
+    }
+
+    public string[] GetPlayerNames(string sortingScoreType)
+    {
+        Init();
+
+        //use playerScores.Keys.OrderByDescending if you want to sort from highest to lowest
+        return playerScores.Keys.OrderBy( n => GetScore(n, sortingScoreType) ).ToArray();
+    }
+
+    public void DEBUG_ADD_KILL_TO_QUILL()
+    {
+        ChangeScore("quill18", "time", 1234);
+    }
+
+    public int GetChangeCounter()
+    {
+        return changeCounter;
+    }
+}
