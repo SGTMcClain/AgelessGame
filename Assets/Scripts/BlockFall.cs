0d95b875c36b1d94ef8771b672a2986c0346a1c2
soliddave84 <soliddave84@users.noreply.github.com>
2015-12-02 23:00:03 -0500

    My level and new script block script
    Please

---
+++ Assets/Scripts/BlockFall.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockFall : MonoBehaviour {
+
+
+    public GameObject block;
+
+    bool triggerActive;
+	// Use this for initialization
+	void Start () {
+        triggerActive = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+    void OnTriggerEnter(Collider box)
+
+    {
+        if (triggerActive == true)
+        {
+            StartCoroutine(waitToFall());
+        }
+    }
+
+   // void OnTriggerExit(Collider box)
+   // {
+    //    block.transform.Translate(0, 5, 0);
+    //}
+
+    IEnumerator waitToFall()
+    {
+
+        yield return new WaitForSeconds(.04f); // this how long before platform breaks/disappears
+
+
+
+
+        block.transform.Translate(0, -5, 0, Camera.main.transform);
+
+
+        triggerActive = false;
+
+    }
+
+}
