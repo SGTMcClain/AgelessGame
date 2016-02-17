985a61fcd6fcd62489377755efa131dc11f1ce68
Nathan McClain <n8mcclain@gmail.com>
2016-02-17 11:50:19 -0500

    Main Menu UI
    Main Menu scene and UI created

--- Assets/Scripts/LoadLevel.cs
+++ Assets/Scripts/LoadLevel.cs
@@ -4,9 +4,20 @@ using System.Collections;
 public class LoadLevel
 : MonoBehaviour {
 
+
 	//Loads Next Level
     public void RetryLevel()
     {
-		Application.LoadLevel(1);
+		Application.LoadLevel(Application.loadedLevel);
     }
+
+	public void StartGame ()
+	{
+		Application.LoadLevel(1);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
 }
