c9fcaa7bc8f217b8e7c6b2916709715f48c70bd4
soliddave84 <soliddave84@users.noreply.github.com>
2016-03-09 19:02:57 -0500

    Level 1 teen done and added Gizmo script to see colliders

---
+++ Assets/Scripts/DrawGizmo.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DrawGizmo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    public bool _bIsSelected = true;
+
+    void OnDrawGizmos()
+    {
+        if (_bIsSelected)
+            OnDrawGizmosSelected();
+    }
+
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(transform.position, 0.1f);  //center sphere
+        if (transform.GetComponent<Renderer>() != null)
+            Gizmos.DrawWireCube(transform.position, transform.GetComponent<Renderer>().bounds.size);
+    }
+}
