ed959ed6bf33e81329694b9240682ce182dd9c68
soliddave84 <soliddave84@users.noreply.github.com>
2016-03-10 05:03:30 -0500

    Placeholder Button Prompts

---
+++ Assets/Scenes/Semester2/DavidLevels 1/IntroLevels/IntroLevelScripts/ButtonPrompt.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonPrompt : MonoBehaviour {
+
+
+    public Canvas m_buttonUI;
+    
+   
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    void OnTriggerEnter(Collider otherPrompt)
+    {
+
+        if (otherPrompt.gameObject.tag =="Player")
+        {
+            
+            m_buttonUI.enabled = true;
+
+        }
+
+
+    }
+
+
+    void OnTriggerExit (Collider otherPrompt)
+    {
+        m_buttonUI.enabled = false;
+    }
+
+}
+
+
+
