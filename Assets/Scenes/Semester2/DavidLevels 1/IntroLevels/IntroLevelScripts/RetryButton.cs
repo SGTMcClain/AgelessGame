c75a96d772e336908b55dda886193d471a3ec2db
soliddave84 <soliddave84@users.noreply.github.com>
2016-03-10 02:29:34 -0500

    Retry button
    For testing purposes. Press R to restart level

---
+++ Assets/Scenes/Semester2/DavidLevels 1/IntroLevels/IntroLevelScripts/RetryButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class RetryButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.R))
+
+        {
+
+            SceneManager.LoadScene("IntroLevel1");
+
+        }
+
+    }
+
+    void RetryLevelButton()
+    {
+        
+    }
+
+}
